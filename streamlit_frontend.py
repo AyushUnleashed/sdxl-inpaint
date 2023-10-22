f741b9ce2fe65f21e84c1a24d6abdac24cd38bc5
AyushUnleashed <myzealouslifeofficial@gmail.com>
2023-10-23 02:42:01 +0530

    modified frontend

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from PIL import Image, ImageDraw
+from utils import base_models_list
 
 # Define the Streamlit app
 st.title("Inpaint Image ")
@@ -12,10 +13,10 @@ st.header("Enter 4 coordinates")
 x1 = st.number_input("X1", value=100)
 y1 = st.number_input("Y1", value=100)
 x2 = st.number_input("X2", value=100)
-y2 = st.number_input("Y2", value=800)
-x3 = st.number_input("X3", value=800)
-y3 = st.number_input("Y3", value=800)
-x4 = st.number_input("X4", value=800)
+y2 = st.number_input("Y2", value=200)
+x3 = st.number_input("X3", value=200)
+y3 = st.number_input("Y3", value=200)
+x4 = st.number_input("X4", value=200)
 y4 = st.number_input("Y4", value=100)
 
 # Add a text input for the prompt
@@ -31,10 +32,14 @@ if uploaded_image is not None:
     polygon_image = image.copy()
     draw = ImageDraw.Draw(polygon_image)
     polygon_coordinates = [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
-    draw.polygon(polygon_coordinates, fill=(0, 0, 0, 100))  # RGBA color with 100 transparency
+    draw.polygon(polygon_coordinates, fill=(0, 0, 0, 50))  # RGBA color with 100 transparency
 
     # Display the image with the polygon
     st.image(polygon_image, caption="Image with Polygon", use_column_width=True)
+
+    # Dropdown to select the base model
+    selected_model = st.selectbox("Select a Model", base_models_list.models_list)
+    base_model = selected_model
 
     # Submit button to print coordinates
     if st.button("Show"):
@@ -46,8 +51,9 @@ if uploaded_image is not None:
 
         st.write(f"Prompt: {prompt}")
         st.write(f"Uploaded Image Path: {uploaded_image_path}")
+        st.write(f"Model used: {selected_model}")
 
     if st.button("Submit"):
         # You can use 'uploaded_image_path' and 'prompt' in your send_api_request function
         from handle_input import send_api_request
-        send_api_request(polygon_coordinates, prompt, uploaded_image_path)
+        send_api_request(polygon_coordinates, prompt, uploaded_image_path, base_model=base_model)
