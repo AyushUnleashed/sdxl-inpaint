0b96c4af9355257137a3c6003aa502097744851e
AyushUnleashed <myzealouslifeofficial@gmail.com>
2023-10-23 00:10:31 +0530

    refactored code to support InpaintRequest Model

--- inpaint_image.py
+++ inpaint_image.py
@@ -1,30 +1,41 @@
 from setup_inpaint_pipeline import setup_pipeline
 from diffusers.utils import load_image
 from create_mask import create_mask
-
+from models.base_model import InpaintRequest
+from utils.utils import decode_base64_image, encode_image
 # width = 1024
 # height = 1024
 
-guidance_scale = 7.5
-num_inference_steps = 30
-inpaint_stength = 0.85
+# guidance_scale = 7.5
+# num_inference_steps = 30
+# inpaint_stength = 0.85
 
 pipe_inpaint = setup_pipeline(base_model_path = "stabilityai/stable-diffusion-xl-base-1.0")
 
-def inpaint_image(prompt ,init_image_path,mask_image_path,inpaint_request=None):
-    init_image = load_image(init_image_path) #.convert("RGB")
-    mask_image = load_image(mask_image_path) #.convert("RGB")
-    image = pipe_inpaint(prompt=prompt, image=init_image, mask_image=mask_image, strength=inpaint_stength, guidance_scale=guidance_scale,num_images_per_prompt=1,num_inference_steps=num_inference_steps).images[0]
+def inpaint_image(inpaint_request: InpaintRequest):
+    # Decode the base64-encoded image
+    init_image = decode_base64_image(inpaint_request.encoded_image)
+    width, height = init_image.size
+    mask_image_path = create_mask(width, height, inpaint_request.coordinates)
+    mask_image = load_image(mask_image_path)
+
+    image = pipe_inpaint(prompt=inpaint_request.prompt,
+                         negative_prompt=inpaint_request.negative_prompt,
+                         image=init_image,
+                         mask_image=mask_image,
+                         strength=inpaint_request.inpaint_strength,
+                         guidance_scale=inpaint_request.guidance_scale,
+                         num_images_per_prompt=inpaint_request.num_images_per_prompt,
+                         num_inference_steps=inpaint_request.num_inference_steps).images[0]
+
     final_image_path = "output.png"
     image.save(final_image_path)
     return final_image_path
 
-def run_inpaint(init_image_path, prompt, coordinates):
-    init_image = load_image(init_image_path)
-    width, height = init_image.size
-    mask_image_path = create_mask(width, height, coordinates)
-    final_image_path = inpaint_image(prompt, init_image_path, mask_image_path)
-    return final_image_path
+def run_inpaint(inpaint_request: InpaintRequest) -> str:
+    final_image_path = inpaint_image(inpaint_request)
+    inpainted_image_encoded = encode_image(final_image_path)
+    return inpainted_image_encoded
 
 
 def main():
@@ -32,7 +43,11 @@ def main():
     init_image_path = "assets/sdxl-text2img.png"
     prompt = "deadpool shooting with guns"
     coordinates = [(100, 100), (800, 100), (800, 800), (100, 800)]
-    run_inpaint(init_image_path, prompt, coordinates)
+    # Create an instance of InpaintRequest
+    request = InpaintRequest(prompt=prompt, encoded_image=init_image_path, coordinates=coordinates)
+
+    # Call the run_inpaint function with the request
+    run_inpaint(request)
 
 if __name__ == "__main__":
     main()
